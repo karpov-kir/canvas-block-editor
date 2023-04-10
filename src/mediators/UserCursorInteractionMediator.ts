f51310c4dfe9c44d1667a185ed95634845f36b4c
Kirill Karpov <karpov.kir.v@gmail.com>
2023-04-10 19:29:00 +0400

    [refactor]: Mediator interface

--- src/mediators/UserCursorInteractionMediator.ts
+++ src/mediators/UserCursorInteractionMediator.ts
@@ -1,11 +1,12 @@
 import { AddBlockCommand } from '../commands/addBlock/AddBlockCommand';
 import { CommandBus } from '../utils/CommandBus';
+import { Mediator } from '../utils/Mediator';
 
 export class CursorEvent {
   constructor(public readonly type: string) {}
 }
 
-export class UserCursorInteractionMediator {
+export class UserCursorInteractionMediator implements Mediator<CursorEvent> {
   constructor(private readonly commandBus: CommandBus) {}
 
   notify(cursorEvent: CursorEvent) {
--- src/mediators/UserKeyboardInteractionMediator.ts
+++ src/mediators/UserKeyboardInteractionMediator.ts
@@ -1,11 +1,12 @@
 import { InputCommand } from '../commands/inputCommand/InputCommand';
 import { CommandBus } from '../utils/CommandBus';
+import { Mediator } from '../utils/Mediator';
 
 export class KeyboardEvent {
   constructor(public readonly type: string, public readonly key: string) {}
 }
 
-export class UserKeyboardInteractionMediator {
+export class UserKeyboardInteractionMediator implements Mediator<KeyboardEvent> {
   constructor(private readonly commandBus: CommandBus) {}
 
   notify(keyboardEvent: KeyboardEvent) {
