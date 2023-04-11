8b08c340718b4ff96d18c4c8cf369b6a110a9a13
Kirill Karpov <karpov.kir.v@gmail.com>
2023-04-11 22:19:19 +0400

    [build]: use swc to run tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,8 @@
-import { Options } from 'esbuild-jest';
 import { Config } from 'jest';
-
-const esbuildOptions: Options = {
-  sourcemap: true,
-};
 
 export default async (): Promise<Config> => ({
   transform: {
-    '^.+\\.tsx?$': ['esbuild-jest', esbuildOptions as Record<string, unknown>],
+    '^.+\\.tsx?$': '@swc/jest',
   },
   testRegex: './src/.*\\.(test|spec)?\\.ts$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
