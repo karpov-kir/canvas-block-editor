fcdf603d11a8620590e5db27d4573fb5e5eb302d
Kirill Karpov <karpov.kir.v@gmail.com>
2023-04-16 11:02:49 +0400

    [refactor]: split RenderService to smaller methods

--- src/commands/render/RenderService.ts
+++ src/commands/render/RenderService.ts
@@ -1,5 +1,5 @@
 import { BlockRect, BlockRectStore, Padding } from '../../stores/BlockRectStore';
-import { BlockStore, BlockType } from '../../stores/BlockStore';
+import { Block, BlockStore, BlockType } from '../../stores/BlockStore';
 import { DocumentStore } from '../../stores/DocumentStore';
 import { Dimensions } from '../../utils/math/Dimensions';
 import { Vector } from '../../utils/math/Vector';
@@ -47,66 +47,88 @@ export class RenderService {
     private readonly documentStore: DocumentStore,
   ) {}
 
-  render() {
-    this.drawer.clear();
+  private renderBlockRect(blockRect: BlockRect, strokeStyle: string) {
+    this.drawer.rect({
+      x: blockRect.position.x,
+      y: blockRect.position.y,
+      width: blockRect.dimensions.width,
+      height: blockRect.dimensions.height,
+      strokeStyle,
+    });
+  }
 
-    const { highlightedBlock, activeBlock, blocks } = this.blockStore;
+  private maybeRenderInactiveBlockRect(block: Block, blockRect: BlockRect) {
+    const { highlightedBlock, activeBlock } = this.blockStore;
+
+    const isBlockFocused = activeBlock?.block.id === block.id;
+    const isBlockHighlighted = highlightedBlock?.id === block.id;
+    const isBlockInactive = !isBlockFocused && !isBlockHighlighted;
+
+    if (isBlockInactive) {
+      this.renderBlockRect(blockRect, 'gray');
+    }
+  }
+
+  private maybeRenderActiveBlockRect() {
+    const { blockRects } = this.blockReactStore;
+    const { activeBlock } = this.blockStore;
+    const activeBlockRect = activeBlock ? blockRects.get(activeBlock.block.id) : undefined;
+
+    if (activeBlockRect) {
+      this.renderBlockRect(activeBlockRect, 'green');
+    }
+  }
+
+  private maybeRenderHighlightedBlockRect() {
+    const { highlightedBlock, activeBlock } = this.blockStore;
     const { blockRects } = this.blockReactStore;
 
-    let nextY = 0;
+    const highlightedBlockRect = highlightedBlock ? blockRects.get(highlightedBlock.id) : undefined;
+    const activeBlockRect = activeBlock ? blockRects.get(activeBlock.block.id) : undefined;
+    const isHighlightedBlockFocused = highlightedBlockRect?.blockId === activeBlockRect?.blockId;
+
+    if (highlightedBlockRect && !isHighlightedBlockFocused) {
+      this.renderBlockRect(highlightedBlockRect, 'red');
+    }
+  }
+
+  private renderBlockContent(block: Block, contentStartX: number, blockContentStartY: number) {
+    const blockHeight = this.drawer.text({
+      ...defaultStyles,
+      width: this.documentStore.maxContentWidth,
+      x: contentStartX,
+      y: blockContentStartY,
+      text: block.type === BlockType.CreateBlock ? 'New +' : block.content,
+    });
+
+    return blockHeight;
+  }
+
+  public render() {
+    this.drawer.clear();
+
+    const { blocks } = this.blockStore;
+
+    let nextBlockContentStartY = 0;
     blocks.forEach((block) => {
       const documentVsContentWithDiff = this.documentStore.dimensions.width - this.documentStore.maxContentWidth;
-      const leftOffset = documentVsContentWithDiff > 0 ? documentVsContentWithDiff / 2 : 0;
-      const blockHeight = this.drawer.text({
-        ...defaultStyles,
-        width: this.documentStore.maxContentWidth,
-        x: leftOffset,
-        y: nextY,
-        text: block.type === BlockType.CreateBlock ? 'New +' : block.content,
-      });
+      const contentStartX = documentVsContentWithDiff > 0 ? documentVsContentWithDiff / 2 : 0;
+
+      const blockHeight = this.renderBlockContent(block, contentStartX, nextBlockContentStartY);
       const blockRect = new BlockRect(
         block.id,
         new Padding(5, 5),
-        new Vector(leftOffset, nextY),
+        new Vector(contentStartX, nextBlockContentStartY),
         new Dimensions(this.documentStore.maxContentWidth, blockHeight),
       );
 
       this.blockReactStore.attach(block.id, blockRect);
+      this.maybeRenderInactiveBlockRect(block, blockRect);
 
-      if (block.id !== highlightedBlock?.id && block.id !== activeBlock?.block.id) {
-        this.drawer.rect({
-          x: blockRect.position.x,
-          y: blockRect.position.y,
-          width: blockRect.dimensions.width,
-          height: blockRect.dimensions.height,
-          strokeStyle: 'gray',
-        });
-      }
-
-      nextY += blockHeight + 1;
+      nextBlockContentStartY += blockHeight + 1;
     });
 
-    const highlightedBlockRect = highlightedBlock ? blockRects.get(highlightedBlock.id) : undefined;
-    const activeBlockRect = activeBlock ? blockRects.get(activeBlock.block.id) : undefined;
-
-    if (activeBlockRect) {
-      this.drawer.rect({
-        x: activeBlockRect.position.x,
-        y: activeBlockRect.position.y,
-        width: activeBlockRect.dimensions.width,
-        height: activeBlockRect.dimensions.height,
-        strokeStyle: 'green',
-      });
-    }
-
-    if (highlightedBlockRect && highlightedBlockRect.blockId !== activeBlockRect?.blockId) {
-      this.drawer.rect({
-        x: highlightedBlockRect.position.x,
-        y: highlightedBlockRect.position.y,
-        width: highlightedBlockRect.dimensions.width,
-        height: highlightedBlockRect.dimensions.height,
-        strokeStyle: 'red',
-      });
-    }
+    this.maybeRenderActiveBlockRect();
+    this.maybeRenderHighlightedBlockRect();
   }
 }
