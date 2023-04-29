151f14e68dc1dd1de168fb426a5523e8db381947
Kirill Karpov <karpov.kir.v@gmail.com>
2023-04-29 20:41:28 +0400

    [refactor]: make RemoveHighlightFromBlockHandler tests easier to follow

--- src/commands/removeHighlightFromBlock/RemoveHighlightFromBlockCommand.test.ts
+++ src/commands/removeHighlightFromBlock/RemoveHighlightFromBlockCommand.test.ts
@@ -7,14 +7,12 @@ describe(RemoveHighlightFromBlockHandler.name, () => {
   it(`removes highlight from the highlighted block on hover and emits ${HighlightRemovedFromBlockEvent.name}`, () => {
     const blockStore = new BlockStore();
     const eventBus = new EventBus();
-    const command = new RemoveHighlightFromBlockCommand(1);
-    const handler = new RemoveHighlightFromBlockHandler(blockStore, eventBus);
     const highlightRemovedFromBlockHandler = jest.fn();
 
     eventBus.subscribe(HighlightRemovedFromBlockEvent, highlightRemovedFromBlockHandler);
     blockStore.add(BlockType.Text);
     blockStore.highlightedBlock = blockStore.blocks.get(1);
-    handler.execute(command);
+    new RemoveHighlightFromBlockHandler(blockStore, eventBus).execute(new RemoveHighlightFromBlockCommand(1));
 
     expect(blockStore.highlightedBlock).toEqual(undefined);
     expect(highlightRemovedFromBlockHandler).toBeCalledWith(expect.any(HighlightRemovedFromBlockEvent));
