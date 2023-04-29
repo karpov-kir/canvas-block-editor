62f9031c5522fddeefa2aa96d13e6b3bd3c2ffcb
Kirill Karpov <karpov.kir.v@gmail.com>
2023-04-29 19:39:21 +0400

    [refactor]: make FocusBlockCommand tests easier to follow

--- src/commands/focusBlock/FocusBlockCommand.test.ts
+++ src/commands/focusBlock/FocusBlockCommand.test.ts
@@ -10,16 +10,14 @@ describe(FocusBlockCommand.name, () => {
     const activeBlockMother = new ActiveBlockMother();
     const blockMother = new BlockMother();
     const blockStore = new BlockStore();
-    const command = new FocusBlockCommand(1);
     const eventBus = new EventBus();
-    const handler = new FocusBlockHandler(blockStore, eventBus);
     const blockFocusedHandler = jest.fn();
 
     blockStore.blocks.set(blockMother.withContent().create().id, blockMother.last);
     blockStore.activeBlock = activeBlockMother.withBlock(blockMother.last).create();
 
     eventBus.subscribe(BlockFocusedEvent, blockFocusedHandler);
-    handler.execute(command);
+    new FocusBlockHandler(blockStore, eventBus).execute(new FocusBlockCommand(1));
 
     expect(blockStore.activeBlock).toEqual(activeBlockMother.last);
     expect(blockFocusedHandler).toBeCalledWith(new BlockFocusedEvent(activeBlockMother.last.block));
