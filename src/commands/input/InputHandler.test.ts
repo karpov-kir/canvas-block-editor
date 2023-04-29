69bc7352e17fcd321147ae2fea0148839a6e6e38
Kirill Karpov <karpov.kir.v@gmail.com>
2023-04-29 19:45:18 +0400

    [refactor]: make InputHandler tests easier to follow

--- src/commands/input/InputHandler.test.ts
+++ src/commands/input/InputHandler.test.ts
@@ -1,5 +1,6 @@
-import { BlockStore, BlockType } from '../../stores/BlockStore';
+import { BlockStore } from '../../stores/BlockStore';
 import { ActiveBlockMother } from '../../testUtils/mothers/ActiveBlockMother';
+import { BlockMother } from '../../testUtils/mothers/BlockMother';
 import { EventBus } from '../../utils/pubSub/EventBus';
 import { InputCommand } from './InputCommand';
 import { InputHandler, InputReceivedEvent } from './InputHandler';
@@ -8,15 +9,15 @@ describe(InputHandler.name, () => {
   it(`adds some input to the currently active block and emits the ${InputReceivedEvent.name}`, () => {
     const blockStore = new BlockStore();
     const eventBus = new EventBus();
-    const handler = new InputHandler(blockStore, eventBus);
-    const command = new InputCommand('Hello world!');
+    const blockMother = new BlockMother();
+    const activeBlockMother = new ActiveBlockMother();
     const inputReceivedHandler = jest.fn();
 
     eventBus.subscribe(InputReceivedEvent, inputReceivedHandler);
-    blockStore.add(BlockType.Text);
-    blockStore.activeBlock = new ActiveBlockMother().create();
+    blockStore.blocks.set(blockMother.create().id, blockMother.last);
+    blockStore.activeBlock = activeBlockMother.withBlock(blockMother.last).create();
 
-    handler.execute(command);
+    new InputHandler(blockStore, eventBus).execute(new InputCommand('Hello world!'));
 
     expect(blockStore.activeBlock).toEqual(
       expect.objectContaining({
